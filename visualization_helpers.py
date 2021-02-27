78784623234abd84112f46c107144c392696f0f6
jeandavidt <38588281+jeandavidt@users.noreply.github.com>
2021-02-27 14:41:09 -0500

    import to db works, viz layout is broken

--- visualization_helpers.py
+++ visualization_helpers.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import glob
+import os.path
+import random
 
 def find_time_columns_to_merge(df):
     categories = {}
@@ -24,3 +27,27 @@ def add_missing_columns(df, needed_names):
 def recombine_times(df):
     categories = find_time_columns_to_merge(df)
     df = add_missing_columns(df)
+
+def create_dummy_polygons(filepath):
+    polygons = {}
+    default_polygon = {"polygonID": None,
+     "name": None,
+     "pop": None,
+     "type": None,
+     "wkt": None,
+     "file": None,
+     "link": None
+     }
+    complete_path = os.path.join(filepath,"*.wkt")
+    polygon_files = glob.glob(complete_path)
+    fields = []
+    for i, file in enumerate(polygon_files):
+        polygon = default_polygon.copy()
+        with open(file, "r") as f:
+            polygon["wkt"] = f.read()
+        polygon["name"] = file.split("/")[-1].split(".")[-2]
+        polygon["pop"] = random.randint(250000, 350000)
+        polygon["type"] = "swrCat"
+        polygon["polygonID"] = polygon["name"]
+        polygons[i] = polygon
+    return pd.DataFrame.from_dict(polygons, orient="index")

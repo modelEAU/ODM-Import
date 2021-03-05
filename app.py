717bbe58c499e001341a3f857aae9ac275e811e2
jeandavidt <jeandavidt@gmail.com>
2021-03-05 20:37:15 +0000

    pep8

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
-from typing import Text
 import dash
-import json
-import pandas as pd
-import plotly.express as px
-import odm
-pd.options.display.max_columns = None
-
-import plotly.express as px
-import plotly.io as pio
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
 from dash.dependencies import Input, Output
 
+import odm
+
+pd.options.display.max_columns = None
 pio.templates.default = "plotly_white"
 # get data
 filename = "Data/Template - Data Model - 20210127.xls"
-xls = pd.read_excel(filename, engine="xlrd", sheet_name=None)
-model = odm.Odm(xls)
+model = odm.Odm()
+model.read_excel(filename)
 samples = model.combine_per_sample()
 geo = model.geo
 map_center = model.map_center
+
 
 def draw_map():
     fig = px.choropleth_mapbox(
@@ -29,25 +27,38 @@ def draw_map():
         featureidkey="properties.polygonID",
         custom_data=["Site.name"],
         color="Site.name",
-        labels={"name":"Sampling Location"},
+        labels={"name": "Sampling Location"},
         center=map_center,
         opacity=0.5,
         mapbox_style="open-street-map",
         zoom=6,
     )
-    fig.update_layout(clickmode="event+select", height=800, legend_orientation="h", legend_yanchor="top", legend_xanchor="left")
+    fig.update_layout(
+        clickmode="event+select",
+        height=800,
+        legend_orientation="h",
+        legend_yanchor="top",
+        legend_xanchor="left"
+    )
     return fig
+
 
 def get_timeseries_names(names):
     return [name for name in names if "date" in name.lower()]
+
+
 def get_values_names(names):
     return [name for name in names if "value" in name.lower()]
 
+
 # Build App
-app = dash.Dash(__name__)#JupyterDash(__name__)
+app = dash.Dash(__name__)  # JupyterDash(__name__)
 app.layout = html.Div([
     dcc.Store(id="site-store"),
-    html.H1("Data exploration - COVID Wastewater data", style={"textAlign":"center"}),
+    html.H1(
+        "Data exploration - COVID Wastewater data",
+        style={"textAlign": "center"}
+    ),
     html.Br(),
     html.Div([
         html.Div([
@@ -76,17 +87,15 @@ app.layout = html.Div([
         dcc.Graph(id='map-1', figure=draw_map()),
     ], style={'width': '45%', 'display': 'inlineBlock', 'float': 'right'}),
     html.Br(),
-
-
 ])
+
 
 # Define callback to update graphs
 @app.callback(
     Output('timeseries-1', 'figure'),
     [Input("x-dropdown-1", "value"),
-    Input("y-dropdown-1", "value"),
-    Input("site-store", "data")]
-)
+     Input("y-dropdown-1", "value"),
+     Input("site-store", "data")])
 def time_series_1(x_col, y_col, data):
     if x_col is None or y_col is None:
         return px.scatter()
@@ -98,6 +107,8 @@ def time_series_1(x_col, y_col, data):
         title=f"{y_col} over {x_col}"
     )
     return fig
+
+
 @app.callback(
     Output("site-store", "data"),
     Input('map-1', 'clickData'))
@@ -106,8 +117,9 @@ def filter_by_clicked_location(clickData):
         return None
     point = clickData["points"][0]
     site_name = point["customdata"][0]
+    return samples.loc[samples["Site.name"] == site_name] \
+        .to_json(date_format='iso')
 
-    return samples.loc[samples["Site.name"]== site_name].to_json(date_format='iso')
 
 @app.callback(
     Output("y-dropdown-1", "options"),
@@ -119,6 +131,7 @@ def update_y_dropdown(data):
         for c in samples.columns if c in get_values_names(df.columns.to_list())
     ]
 
+
 @app.callback(
     Output("x-dropdown-1", "options"),
     Input('site-store', 'data'))
@@ -126,9 +139,10 @@ def update_x_dropdown(data):
     df = samples if data is None else pd.read_json(data)
     return [
         {'label': c, 'value': c}
-        for c in samples.columns if c in get_timeseries_names(df.columns.to_list())
+        for c in samples.columns
+        if c in get_timeseries_names(df.columns.to_list())
     ]
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True) # inline
+    app.run_server(debug=True)  # inline
---
+++ modeleau_lab_data.py
@@ -0,0 +1,213 @@
+import pandas as pd
+import datetime as dt
+
+
+def replace_excel_dates(series):
+    return series.apply(
+        lambda x: pd.to_timedelta(x, unit='d') +
+        dt.datetime(1899, 12, 30) if isinstance(x, float) else x
+    )
+
+
+def clean_up(df):
+    for col in df.columns.to_list():
+        df[col] = pd.to_datetime(replace_excel_dates(df[col])) \
+            if "date" in col.lower() else df[col]
+        df[col] = df[col].apply(lambda x: None if x in ["None", ""] else x)
+        if col == "Measurement":
+            df[col] = df[col].apply(lambda x: x.replace("*", "").strip())
+        if "Unnamed" in col:
+            del df[col]
+    return df
+
+
+default_sample = {
+        "sampleID": None,
+        "siteID": None,
+        "reporterID": "MaryamTohidi",
+        "dateTime": None,
+        "dateTimeStart": None,
+        "dateTimeEnd": None,
+        "type": "pstGrit",
+        "collection": "cpTP24h",
+        "preTreatment": None,
+        "pooled": None,
+        "children": None,
+        "parent": None,
+        "sizeL": 1,
+        "fieldSampleTempC": 4,
+        "shippedOnIce": "Yes",
+        "storageTempC": 4,
+        "qualityFlag": "NO",
+        "notes": "",
+        "index": 1
+    }
+
+default_measurement = {
+    "uWwMeasureID": None,
+    "WwMeasureID": None,
+    "reporterID": "MaryamTohidi",
+    "sampleID": None,
+    "labID": "ModelEau_lab",
+    "assayMethodID": None,
+    "analysisDate": None,
+    "reportDate": None,
+    "fractionAnalyzed": None,
+    "type": None,
+    "value": None,
+    "unit": None,
+    "aggregation": "single",
+    "index": 0,
+    "qualityFlag": "NO",
+    "accessToPublic": "YES",
+    "accessToAllOrg": "YES",
+    "accessToPHAC": "YES",
+    "accessToLocalHA": "YES",
+    "accessToProvHA": "YES",
+    "accessToOtherProv": "YES",
+    "accessToDetails": "YES",
+    "notes": None,
+}
+
+measurement_dico = {
+    "Turbidity": "wqTurb",
+    "covN1": "covN1",
+    "covN2": "covN2",
+    "covN3": "covN3",
+    "covE": "covE",
+    "covRdRp": "covRdRp",
+    "nPMMoV": "nPMMoV",
+    "nCrA": "nCrA",
+    "nBrsv": "nBrsv",
+    "TS": "wqTS",
+    "TSS": "wqTSS",
+    "VSS": "wqVSS",
+    "COD": "wqCOD",
+    "P": "wqOPhos",
+    "NH4": "wqNH4N",
+    "TN": "wqTN",
+    "pH": "wqPh",
+    "Conductivity": "wqCond",
+}
+
+
+def check_if_grab(row):
+    return pd.isna(row["Sample end date"]) or pd.isna(row["Sample start date"])
+
+
+def build_sample_id(measurement_sheet_row):
+    is_grab = check_if_grab(measurement_sheet_row)
+    if is_grab:
+        id_date = measurement_sheet_row["Date (enter end date here)"]
+    else:
+        id_date = measurement_sheet_row["Sample start date"]
+
+    id_date = id_date.strftime(r"%Y-%m-%d")
+
+    if "sampleType" in measurement_sheet_row:
+        sample_type = measurement_sheet_row["sampleType"]
+    else:
+        sample_type = default_sample["type"]
+
+    if "sampleIndex" in measurement_sheet_row:
+        sample_index = measurement_sheet_row["sampleIndex"]
+    else:
+        sample_index = default_sample["index"]
+
+    site_id = measurement_sheet_row["siteID"]
+    return ("_").join([site_id, id_date, sample_type, str(sample_index)])
+
+
+def build_measurement_id(measurement_sheet_row):
+    sample_id = build_sample_id(measurement_sheet_row)
+    lab_id = default_measurement["labID"]
+    measurement_type = measurement_dico[measurement_sheet_row["Measurement"]]
+    date = measurement_sheet_row["Analysis Date"].strftime(r"%Y-%m-%d")
+    if "measurementIndex" in measurement_sheet_row:
+        measurement_index = measurement_sheet_row["measurementIndex"]
+    else:
+        measurement_index = default_measurement["index"]
+    return ("_").join([
+        sample_id,
+        lab_id,
+        measurement_type,
+        date,
+        str(measurement_index)
+    ])
+
+
+def create_sample_row(row):
+    new_sample = default_sample.copy()
+    new_sample["sampleID"] = build_sample_id(row)
+    is_grab = check_if_grab(row)
+    if is_grab:
+        new_sample["dateTime"] = row["Date (enter end date here)"]
+    else:
+        new_sample["dateTimeEnd"] = row["Sample end date"]
+        new_sample["dateTimeStart"] = row["Sample start date"]
+
+    if "reporterID" in row:
+        new_sample["reporterID"] = row["reporterID"]
+    if "sampleIndex" in row:
+        new_sample["index"] = row["sampleIndex"]
+
+    new_sample["siteID"] = row["siteID"]
+
+    return pd.Series(new_sample)
+
+
+def get_samples_from_lab_sheet(df):
+    samples = df.apply(lambda x: create_sample_row(x), axis=1)
+    samples = samples.drop_duplicates()
+    samples.reset_index(drop=True, inplace=True)
+    return samples
+
+
+def create_measurement_row(row):
+    new_measurement = default_measurement.copy()
+    new_measurement["sampleID"] = build_sample_id(row)
+    new_measurement["uWwMeasureID"] = build_measurement_id(row)
+    new_measurement["analysisDate"] = row["Analysis Date"]
+    new_measurement["type"] = measurement_dico[row["Measurement"]]
+    new_measurement["value"] = row["Value"]
+    new_measurement["unit"] = row["Unit"]
+    new_measurement["fractionAnalyzed"] = row["fraction analyzed"]
+    new_measurement["qualityFlag"] = row["qualityFlag"]
+    new_measurement["notes"] = row["notes"]
+
+    if "index" in row:
+        new_measurement["index"] = row["index"]
+    return pd.Series(new_measurement)
+
+
+def edit_index_in_id(row):
+    current_index = row["uWwMeasureID"]
+    row["uWwMeasureID"] = current_index[:-1] + str(row["index"])
+    return row
+
+
+def build_missing_indices(df):
+    uniques = df["uWwMeasureID"].drop_duplicates()
+    for _, unique in enumerate(uniques):
+        replicates = df.loc[df["uWwMeasureID"] == unique]
+        indices = [x+1 for x in range(len(replicates))]
+        df.loc[df["uWwMeasureID"] == unique, ["index"]] = indices
+    df = df.apply(lambda x: edit_index_in_id(x), axis=1)
+    return df
+
+
+def get_measurements_from_lab_sheet(df):
+    measurements = df.apply(lambda x: create_measurement_row(x), axis=1)
+    measurements.reset_index(drop=True, inplace=True)
+    if len(measurements.loc[measurements["index"] == 0]) > 0:
+        measurements = build_missing_indices(measurements)
+    return measurements
+
+
+if __name__ == "__main__":
+    path = "Data/Lab/COVIDProject_Lab Measurements.xlsx"
+    sheet_name = "Lab analyses"
+    df = pd.read_excel(path, sheet_name=sheet_name)
+    df = clean_up(df)
+    samples = get_samples_from_lab_sheet(df)
+    measurements = get_measurements_from_lab_sheet(df)

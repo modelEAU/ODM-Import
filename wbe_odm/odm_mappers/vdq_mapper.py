0c2fb6ffd2f66f4fbf6f1b9ca29e2f8898566ccf
jeandavidt <jeandavidt@gmail.com>
2021-05-05 09:48:43 -0400

    Added rain mapper to vdq_mapper.py

--- wbe_odm/odm_mappers/vdq_mapper.py
+++ wbe_odm/odm_mappers/vdq_mapper.py
@@ -1,7 +1,10 @@
 import os
 import pandas as pd
 import numpy as np
-from wbe_odm.odm_mappers import mcgill_mapper as mcm
+from wbe_odm.odm_mappers import(
+    mcgill_mapper as mcm,
+    base_mapper as bm
+)
 
 
 directory = os.path.dirname(__file__)
@@ -138,7 +141,9 @@ class VdQSensorsMapper(mcm.McGillMapper):
             mcm.excel_style(i+1)
             for i, _ in enumerate(df.columns.to_list())
         ]
-        df = df.loc[~df["D"].astype(str).str.lower().isin(["moyen", "max", "min"])]
+        df = df.loc[
+            ~df["D"].astype(str)
+            .str.lower().isin(["moyen", "max", "min"])]
         df = df.dropna(how="all")
         date_cols = ["A", "B", "C", "D"]
         numeric_cols = [col for col in df.columns if col not in date_cols]
@@ -154,10 +159,61 @@ class VdQSensorsMapper(mcm.McGillMapper):
         self.site_measure = site_measure
         return
 
+rain_default_site_measurement = {
+    "siteMeasureID": None,
+    "siteID": None,
+    "reporterID": "CityQC",
+    "instrumentID": "Pluvio_VilledeQuebec",
+    "dateTime": None,
+    "type": "envRnF",
+    "aggregation": "single",
+    "aggregationDesc": "Cumulative rainfall in one day.",
+    "value": None,
+    "unit": "mm",
+    "accessToPublic": "No",
+    "accessToAllOrgs": "No",
+    "accessToPHAC": "No",
+    "accessToLocalHA": "No",
+    "accessToProvHA": "No",
+    "accessToOtherProv": "No",
+    "accessToDetails": "No",
+    "notes": ""
+}
 
-if __name__ == "__main__":
-    lab_path = "Data/VdQ/Échantillonnage COVID ULaval.xlsx"
-    sensors_path = "Data/VdQ/DonneesULaval.xlsx"
-    mapper = VdQSensorsMapper()
-    mapper.read(sensors_path)
-    print("ok")
+
+class VdQRainMapper(bm.BaseMapper):
+    def read(self, filepath):
+        xl_file = pd.ExcelFile(filepath)
+        odm_name = self.conversion_dict["site_measure"]["odm_name"]
+        second_sheet_name = xl_file.sheet_names[1]
+        xls = pd.read_excel(
+            filepath,
+            sheet_name=[second_sheet_name],
+            header=0,
+            skiprows=[1])
+
+        df = xls[second_sheet_name][['Date', 'Pluvio', 'Hauteur totale (mm)']]
+        df = df.dropna(subset=['Date'], axis=0)
+        df = df.reset_index(drop=True)
+
+        df['siteID'] = pd.Series(
+            map(lambda x: 'QC_wstation_' + str(x), df['Pluvio']))
+        del df['Pluvio']
+
+        for k, v in rain_default_site_measurement.items():
+            if v is not None:
+                df[k] = v
+
+        df['siteMeasureID'] = df['siteID'] + "_" + df['type'] + "_" \
+            + df["Date"].dt.strftime('%Y-%m-%d')
+        df['siteMeasureID'].astype(str)
+        df.rename(columns={
+            "Hauteur totale (mm)": "value",
+            "Date": "dateTime"
+            }, inplace=True)
+        df = df[list(rain_default_site_measurement.keys())]
+        site_measure = self.type_cast_table(odm_name, df)
+        self.site_measure = site_measure
+
+    def validates(self):
+        return True
